ad79ef6fdfe54f9a35ce1da8404e4e704017a210
deliciousshingo <67893129+deliciousshingo@users.noreply.github.com>
2020-09-01 19:42:52 +0900

    メンター酒徳　devise入れ直し

--- db/migrate/20200901101705_devise_create_users.rb
+++ db/migrate/20200901101705_devise_create_users.rb
@@ -11,7 +11,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       t.string :last_name_reading,  null: false
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
-      t.string :birth_date, null: false
+      t.date :birth_date, null: false
 
       ## Recoverable
       t.string   :reset_password_token
