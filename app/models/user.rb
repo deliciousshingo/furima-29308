8e54ef12a7bc530e3883411e6259831e2b513315
deliciousshingo <67893129+deliciousshingo@users.noreply.github.com>
2020-09-03 12:46:52 +0900

    Userバリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   with_options presence: true do
-    validates :nickname, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
-    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
-    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
-    validates :first_name_reading, format: { with: /\A[ァ-ヶー－]+\z/ }
-    validates :last_name_reading, format: { with: /\A[ァ-ヶー－]+\z/ }
-    validates :email, format: { with: /[a-z\d]{6,}@.+/i }
-    validates :password, format: { with:/\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
-    validates :encrypted_password, format: { with:/\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
+    validates :nickname
+    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'is invalid. Input full-width characters.' }
+    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'is invalid. Input full-width characters.' }
+    validates :first_name_reading, format: { with: /\A[ァ-ヶー－]+\z/, message: 'is invalid. Input full-width katakana characters.' }
+    validates :last_name_reading, format: { with: /\A[ァ-ヶー－]+\z/, message: 'is invalid. Input full-width katakana characters.' }
+    validates :email, format: { with: /[a-z\d]@+/i }
+    validates :password, format: { with: /[a-z\d]/i }
+    validates :encrypted_password, format: { with: /[a-z\d]/i }
   end
 end
