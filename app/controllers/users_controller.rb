49e90fef1b72fab7cbe056e31a011ebacb2454c6
deliciousshingo <67893129+deliciousshingo@users.noreply.github.com>
2020-09-06 17:20:32 +0900

    usersコントローラー作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,22 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    User.create(user_params)
+  end
+
+  def destroy
+    user = User.find(params[:id])
+    user.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:nickname, :first_name, :last_name, :first_name_reading, :last_name_reading, :email, :encrypted_password, :birth_date)
+  end
 end
+
