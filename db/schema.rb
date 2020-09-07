6ebb83789caf9ceb6f62b9d454fcd314f59c17a2
deliciousshingo <67893129+deliciousshingo@users.noreply.github.com>
2020-09-07 12:57:12 +0900

    itemsテーブル修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_03_064408) do
+ActiveRecord::Schema.define(version: 2020_09_07_034500) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -43,6 +43,8 @@ ActiveRecord::Schema.define(version: 2020_09_03_064408) do
     t.integer "region_id", null: false
     t.integer "ship_date_id", null: false
     t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_items_on_user_id"
   end
 
